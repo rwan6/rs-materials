cca4e8e9d2342c01a84e856f93d16dd5b79bab30
Rahul Prabala <rprabala@stanford.edu>
2016-12-03 19:05:46 -0800

    split 0 and 1 for equality

--- scripts/image_classification.py
+++ scripts/image_classification.py
@@ -11,8 +11,10 @@ from sklearn.model_selection import train_test_split
 import glob
 # test = mimg.imread("/home/rprabala/Downloads/pic_2.bmp")
 folder_name = '../rs-materials/rs_resize_pics/tiled/*.bmp'
-images_list = []
-labels_list = []
+images1_list = []
+labels1_list = []
+images0_list = []
+labels0_list = []
 for filename in glob.glob(folder_name):
   if "rgb" not in filename:
     continue
@@ -23,27 +25,38 @@ for filename in glob.glob(folder_name):
   cur_depth_image = plt.imread(depth)
   concat = np.append(cur_rgb_image, cur_depth_image)
 
-  images_list.append(concat)
-  labels_list.append(cur_label)
+  if cur_label == 0:
+  	images0_list.append(concat)
+  	labels0_list.append(cur_label)
+  else:
+  	images1_list.append(concat)
+  	labels1_list.append(cur_label)
 
-featArray = np.array(images_list)
-labelsArray = np.array(labels_list)
-numSamples = len(images_list)
+feat0Array = np.array(images0_list)
+labels0Array = np.array(labels0_list)
 
-Xtrain, Xtest, ytrain, ytest = train_test_split(
-  featArray, labelsArray, test_size=0.10, random_state=1)
+feat1Array = np.array(images1_list)
+labels1Array = np.array(labels1_list)
+
+numSamples = len(images0_list) + len(images1_list)
+
+X0train, X0test, y0train, y0test = train_test_split(
+  feat0Array, labels0Array, test_size=0.10, random_state=1)
+
+X1train, X1test, y1train, y1test = train_test_split(
+  feat1Array, labels1Array, test_size=0.10, random_state=1)
 
 print("Now training classifier")
 
 # Create a classifier: a support vector classifier
 classifier = svm.SVC(gamma=0.001)
 
-classifier.fit(Xtrain, ytrain)
+classifier.fit(np.append(X0train, X1train), np.append(y0train, y1train))
 
 print("Now predicting with classifier")
 
-expected = ytest
-predicted = classifier.predict(Xtest)
+expected = np.append(y0test, y1_test)
+predicted = classifier.predict(np.append(X0test, X1test))
 
 print("Classification report for classifier %s:\n%s\n"
      % (classifier, metrics.classification_report(expected, predicted)))
